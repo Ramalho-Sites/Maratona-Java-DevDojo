907f07aeb8b945473f4b7e09f87479f0b47511c0
Ramalho-Sites <daviramalho101@gmail.com>
2025-11-12 20:44:25 -0300

    feat:Aulas de TiposPrimitivos Feitas

---
+++ src/devdojo/aulas/TiposPrimitivos/Aula10.java
@@ -0,0 +1,8 @@
+package devdojo.aulas.TiposPrimitivos;
+
+public class Aula10 {
+    static void main(String[] args) {
+        int age = 18;
+        System.out.println("Age is " +age+" years");
+    }
+}
---
+++ src/devdojo/aulas/TiposPrimitivos/Aula12.java
@@ -0,0 +1,20 @@
+package devdojo.aulas.TiposPrimitivos;
+
+public class Aula12 {
+    static void main(String[] args) {
+        int age = 18;
+        long bigNumebers = 10000000;
+        double wageDouble = 2000;
+        float  wageFloat = (float) 2500.00;
+        byte ageByte = 18;
+        short ageShort = 10;
+        boolean True = true;
+        boolean False = false;
+        char character = '\u0041';
+
+        System.out.println("Age is " + age + " years");
+        System.out.println(True);
+        System.out.println("Char" + character);
+        System.out.println(wageFloat);
+    }
+}
